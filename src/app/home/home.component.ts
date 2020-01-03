b7f148d4c9abbf64449a64fa742afa2e1c0bfb6b
brunda <tinavarma301@gmail.com>
2020-01-03 17:14:26 +0530

    changed json prac

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+x:string="RANDOM DATA";
+y:number=1;
+z=true;
   constructor() { }
 
   ngOnInit() {
+    this.x=this.x+ " new";
+  }
+  fnclick(){
+    this.x=this.x+ " new";
+
   }
 
 }
